c8cd8fc9d156a1f34d99ace7465193b6d7ac01bd
Joshua Steele <jsteele@nitrocade.com>
2023-02-26 17:52:06 -0700

    Basic page and animation

--- apps/dashkit/src/pages/index.tsx
+++ apps/dashkit/src/pages/index.tsx
@@ -1,12 +1,47 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
 import { projectTitle } from '@/config'
+import { animate, motion, MotionProps, useScroll, useTransform, cubicBezier } from 'framer-motion'
+
+import BaseRoomLogo from "@/svg/room.svg"
+
+import { HiChevronUp as BaseHiChevronUp } from 'react-icons/hi2';
+import React, { useRef } from 'react'
+
+const HiChevronUp  = ({...props}) => (
+  <motion.i {...props}>
+    <BaseHiChevronUp />
+  </motion.i>
+)
+
+// eslint-disable-next-line react/display-name
+const RoomLogo = motion(React.forwardRef<HTMLDivElement>((props, ref) => <div ref={ref}>
+  <BaseRoomLogo {...props} />
+</div>))
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const introRef = useRef(null)
+  const {scrollYProgress} = useScroll({
+    offset: [
+      "start start",
+      "end end"
+    ]
+  })
+
+  const scale = useTransform(
+    scrollYProgress,
+    [0, 1],
+    [1, 5],
+  )
+  
+  const opacity = useTransform(
+    scrollYProgress,
+    [0, 1],
+    [1, 0],
+  )
+
   return (
     <>
       <Head>
@@ -15,11 +50,36 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className={"flex justify-center items-center font-semibold text-6xl h-screen w-screen fixed"}>
-        Test
+      <div 
+        className={"flex flex-col justify-center items-center font-semibold text-6xl h-screen w-screen fixed"}
+        style={{
+          background: "var(--color-primary)",
+          color: "var(--color-primary-contrast)",
+        }}
+      >
+        <RoomLogo style={{ opacity, scale }} />
       </div>
-      <div className={"translate-y-3/4 w-screen h-screen bg-slate-200"}>
-        Hello
+      <div className={"flex flex-col items-center w-screen h-screen rounded-t-3xl"} style={{translate: "0 calc(100vh - 400px)"}}>
+        <motion.p className='text-center text-6xl pb-16' style={{color: "var(--color-primary-contrast)", opacity }}>
+          My Room
+        </motion.p>
+      <div ref={introRef} className={"flex flex-col items-center bg-slate-200 w-screen h-full"}>
+        <HiChevronUp 
+          className={"text-slate-500 text-4xl my-8"}
+          animate={{ y: [0, 10, 0] }}
+          transition={{
+            duration: 3,
+            repeat: Infinity,
+            ease: "easeInOut"
+          }}
+        />
+
+        <p className='text-center'>
+          My Room - &copy; 2023<br />
+          Made for the ETHDenver #BUIDLathon 2023<br />
+          By Dino, Liang, Geoffrey and Josh.
+        </p>
+      </div>
       </div>
     </>
   )
