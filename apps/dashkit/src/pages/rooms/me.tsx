bb0461249eb8fd6bcd68771ed0fc3d810b68fa5f
Joshua Steele <jsteele@nitrocade.com>
2023-03-04 02:35:09 -0700

    Wrap sign in redirect in useEffect

--- apps/dashkit/src/pages/rooms/me.tsx
+++ apps/dashkit/src/pages/rooms/me.tsx
@@ -1,10 +1,11 @@
-import { NextPage } from "next"
-
+import { useEffect } from "react"
 import { useRouter } from "next/router"
 import { useAccount } from "wagmi"
 import { useUniversalResolver } from "@/utils"
 
 import { HiPaperAirplane } from "react-icons/hi2"
+
+import { NextPage } from "next"
 
 const RoomOfAccount: NextPage = () => {
   const { isConnected, address } = useAccount()
@@ -12,9 +13,11 @@ const RoomOfAccount: NextPage = () => {
 
   const router = useRouter()
 
-  if (!isConnected) {
-    router.push('/')
-  }
+  useEffect(() => {
+    if (!isConnected) {
+      router.push('/')
+    }
+  }, [isConnected, router])
 
   return <>
     <h1>Welcome 
