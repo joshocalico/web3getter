18a53cf308d1a888d31e5d18c520699ae45da9b4
jajablinky <gmillar6@gmail.com>
2023-03-04 22:08:23 -0800

    widget format

--- apps/dashkit/src/components/DragGrid.tsx
+++ apps/dashkit/src/components/DragGrid.tsx
@@ -5,25 +5,43 @@ import {
   useSensor,
   useSensors,
   closestCenter,
-} from "@dnd-kit/core";
-import { SortableContext } from "@dnd-kit/sortable";
-import { Showcase } from "@/components";
-import BaseWidget from "./widgets/BaseWidget";
+} from '@dnd-kit/core';
+import { SortableContext } from '@dnd-kit/sortable';
+import { Showcase } from '@/components';
+import BaseWidget from './widgets/BaseWidget';
 
 const DragGrid = () => {
-  const sensors = useSensors(useSensor(PointerSensor), useSensor(TouchSensor));
+  const sensors = useSensors(
+    useSensor(PointerSensor),
+    useSensor(TouchSensor)
+  );
 
   return (
     <div className="w-full max-w-2xl">
-      <DndContext sensors={sensors} collisionDetection={closestCenter}>
+      <DndContext
+        sensors={sensors}
+        collisionDetection={closestCenter}
+      >
         <Showcase data={{}} />
         <SortableContext items={[]}>
-          <div style={{ display: "grid", gridTemplateColumns: "repeat(3, 1fr)", gridAutoFlow: "dense", gridAutoRows: "3fr" }}>
+          <div
+            style={{
+              display: 'grid',
+              gridTemplateColumns: 'repeat(3, 1fr)',
+              gridAutoFlow: 'dense',
+              gridAutoRows: '3fr',
+            }}
+          >
             <BaseWidget size="small" id="tiny" />
-            <BaseWidget size="big" id="big" />
+            <BaseWidget size="small" id="tiny" />
             <BaseWidget size="doubleTall" id="slender" />
             <BaseWidget size="doubleWide" id="wide" />
-            <BaseWidget size="tripleWide" id="3wide" />
+            <BaseWidget size="small" id="tiny" />
+            <BaseWidget size="doubleWide" id="wide" />
+            <BaseWidget size="small" id="tiny" />
+            <BaseWidget size="doubleTall" id="slender" />
+            <BaseWidget size="doubleTall" id="slender" />
+            <BaseWidget size="small" id="tiny" />
           </div>
         </SortableContext>
       </DndContext>
