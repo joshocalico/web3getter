47657b3be47bf01603f1c9618ab723f695b32e44
Joshua Steele <jsteele@nitrocade.com>
2023-03-04 21:38:15 -0700

    Prettier

--- apps/dashkit/src/pages/rooms/me.tsx
+++ apps/dashkit/src/pages/rooms/me.tsx
@@ -9,13 +9,12 @@ import { HiPaperAirplane } from "react-icons/hi2";
 
 import { NextPage } from "next";
 
-
 const RoomOfAccount: NextPage = () => {
   const { status, address: _address } = useAccount();
   const { name: _name, isLens } = useUniversalResolver(_address as string);
 
-  const name = useDefaultOnHydration(_name, "unknown")
-  const address = useDefaultOnHydration(_address as string, "unknown")
+  const name = useDefaultOnHydration(_name, "unknown");
+  const address = useDefaultOnHydration(_address as string, "unknown");
 
   const router = useRouter();
 
@@ -23,31 +22,33 @@ const RoomOfAccount: NextPage = () => {
     if (status !== "connected") {
       router.push("/");
     }
-    
   }, [status, router]);
 
   return (
     <>
       <Navigation />
-    <div className="flex flex-col justify-center items-center">
-    <div className="flex">
-      <Greeter me name={name ?? `${address?.slice(0, 8) ?? "unknown"}...`} />
-      {isLens && (
-        <Button>
-          <HiPaperAirplane /> Share to Lenster
-        </Button>
-      )}
-    </div>
+      <div className="flex flex-col justify-center items-center">
+        <div className="flex">
+          <Greeter
+            me
+            name={name ?? `${address?.slice(0, 8) ?? "unknown"}...`}
+          />
+          {isLens && (
+            <Button>
+              <HiPaperAirplane /> Share to Lenster
+            </Button>
+          )}
+        </div>
 
-      <DragGrid />
+        <DragGrid />
       </div>
       <p className="text-center p-4 font-sans font-thin">
-            My Room - &copy; 2023
-            <br />
-            Made with ♥ for ETHDenver #BUIDLathon 2023
-            <br />
-            By Dino, Liang, Geoffrey and Josh.
-          </p>
+        My Room - &copy; 2023
+        <br />
+        Made with ♥ for ETHDenver #BUIDLathon 2023
+        <br />
+        By Dino, Liang, Geoffrey and Josh.
+      </p>
     </>
   );
 };
